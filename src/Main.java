b8ed5b6d8c84ed38e9d55532023141d53066c9a9
Administrator <3542183560@qq.com>
2024-09-16 23:39:45 +0800

    第一次上传

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int p=1,k=0;
+        int m=1;
+        while (m<=n){
+            m=m*p;
+            k+=1;
+
+        }
+        System.out.println(k+p);
+
+
+
+    }
+}
